342f7f0a780510c646ce91e9383c898e0aaf77c3
Julia Hormuth <jvhormuth@gmail.com>
2022-10-02 22:49:45 -0300

    created events

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import TemplateExpressions from "./components/TemplateExpressions";
+import FirstComponent from "./components/FirstComponent";
+import Events from "./components/Events";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <TemplateExpressions />
+      <FirstComponent />
+      <Events />
     </div>
   );
 }
