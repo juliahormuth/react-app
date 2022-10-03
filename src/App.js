618f373bf3e176a7eec09d2d9d68a0d8e298a4ac
Julia Hormuth <jvhormuth@gmail.com>
2022-10-03 19:43:30 -0300

    props with arrays

--- src/App.js
+++ src/App.js
@@ -6,10 +6,17 @@ import ManageData from "./components/ManageData";
 import ListRender from "./components/ListRender";
 import ConditionalRender from "./components/ConditionalRender";
 import ShowUserName from "./components/ShowUserName";
+import CarDetails from "./components/CarDetails";
 
 import "./App.css";
 
 function App() {
+  const cars = [
+    { id: 1, brand: "Ferrari", color: "Amarelo", newCar: true, km: 0 },
+    { id: 2, brand: "BMW", color: "Preto", newCar: false, km: 1000 },
+    { id: 3, brand: "VM", color: "Roxo", newCar: false, km: 23345 },
+  ];
+
   return (
     <div className="App">
       <TemplateExpressions />
@@ -26,7 +33,22 @@ function App() {
       <ManageData />
       <ListRender />
       <ConditionalRender />
+      {/* props */}
       <ShowUserName name="Julia" />
+      {/* destructuring */}
+      <CarDetails brand="VM" km={1000} color="Azul" />
+      {/* reaproveitando */}
+      <CarDetails brand="Ford" km={2000} color="Vermelho" />
+      <CarDetails brand="Fiat" km={4000} color="Verde" newCar={true} />
+      {/* reaproveitando */}
+      {cars.map((car) => (
+        <CarDetails
+          brand={car.brand}
+          color={car.color}
+          km={car.km}
+          newCar={car.newCar}
+        />
+      ))}
     </div>
   );
 }
